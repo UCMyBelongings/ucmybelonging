dd8241332d7db1b0f74eeec87423a285281b4f23
7174Andy <hee031011@likelion.org>
2023-04-12 19:11:13 -0700

    Creation of database schema

---
+++ models/Post.js
@@ -0,0 +1,55 @@
+const mongoose = require("mongoose");
+
+const PostSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "user",
+  },
+  text: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+  },
+  avatar: {
+    type: String,
+  },
+  foundedDate: {
+    type: Date,
+    required: true,
+  },
+  location: {
+    type: String,
+    required: true,
+  },
+  found: {
+    type: Boolean,
+    required: true,
+  },
+  comment: [
+    {
+      user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "user",
+      },
+      text: {
+        type: String,
+        required: true,
+      },
+      name: {
+        type: String,
+      },
+      date: {
+        type: Date,
+        default: Date.now,
+      },
+    },
+  ],
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = Post = mongoose.model("post", PostSchema);
---
+++ models/Profile.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const ProfileSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "user",
+  },
+  contact: {
+    type: String,
+    required: true,
+  },
+  year: {
+    type: int,
+  },
+  college: {
+    type: String,
+  },
+});
+
+module.exports = Profile = mongoose.model("profile", ProfileSchema);
