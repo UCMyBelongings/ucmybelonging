995ec52457592b98bb7a4f8eff85d0d2f885603f
jhuynh <joh022@ucsd.edu>
2023-05-09 19:03:06 -0700

    first commit

--- client/src/Homepage.jsx
+++ client/src/Homepage.jsx
@@ -1,27 +1,31 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./homepage.css";
+import logo from "./img/logo.png";
 
 function Homepage() {
   return (
-    <div className="homepage">
-      <div className="logo-container">
-        <img src="path/to/logo.png" alt="logo" />
-      </div>
-      <h3 className="title">Welcome to Our Website</h3>
-      <nav className="navbar">
-        <ul>
-          <li>
-            <Link to="/about-us">About Us</Link>
-          </li>
-          <li>
-            <Link to="/login">Login</Link>
-          </li>
-          <li>
-            <Link to="/donate">Donate</Link>
-          </li>
-        </ul>
-      </nav>
+    <div>
+      <section class="colored-section-1" id="title">
+        <nav className="navbar">
+          <ul>
+            <li>
+              <Link to="/about-us">About Us</Link>
+            </li>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+            <li>
+              <Link to="/donate">Donate</Link>
+            </li>
+          </ul>
+        </nav>
+      </section>
+      <img src={logo} className="logo" />
+      <section class="colored-section-2" id="title">
+        {/* <h3 className="title">Welcome to Our Website</h3> */}
+      </section>
+      <button className="btn">Start Browsing</button>
     </div>
   );
 }
